17ace05692d376bfd28748ae3256432c70b56066
Yevhenii Pozniak <jekamanu@gmail.com>
2025-05-19 13:22:02 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import "./App.css";
+import { Layout } from "./components/Layout/Layout";
+import { AppBar } from "./components/AppBar/AppBar";
+import { TaskForm } from "./components/TaskForm/TaskForm";
+import { TaskList } from "./components/TaskList/TaskList";
 
-function App() {
-  return <></>;
-}
-
-export default App;
+export const App = () => {
+  return (
+    <Layout>
+      <AppBar />
+      <TaskForm />
+      <TaskList />
+    </Layout>
+  );
+};
